9e399ec6d742d790304b1470d2ad2f6477277936
noanswerss <ciprianvlad888@gmail.com>
2022-08-28 09:37:16 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Saved successfully"
+  })
+});
+
+app.get('/api/stuff', (req, res, next) => {
   const stuff = [
     {
         _id: 'string',
@@ -30,5 +37,12 @@ app.use('/api/stuff', (req, res, next) => {
   ];
   res.status(200).json(stuff);
 });
+
+// Create User model
+
+// Create User Route
+
+// Create User Controller
+
 
 module.exports = app;
