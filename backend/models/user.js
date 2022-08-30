9266e6028c72246f9f47ea4f2ec9fe3dc3850352
noanswerss <ciprianvlad888@gmail.com>
2022-08-30 21:39:26 +0100

    finished user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
     password: {type:String ,required: true},
    
     email: {type: String , required : true , unique : true},  
-})
+});
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
 
